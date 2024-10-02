28fb813325cf2768c22762db19e47e4a28f520d7
Brayden Tomlinson <humortronic@gmail.com>
2024-10-02 17:54:27 +0930

    Added changes to code

--- ex00/megaphone.cpp
+++ ex00/megaphone.cpp
@@ -6,7 +6,7 @@
 /*   By: btomlins <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/01 16:38:37 by btomlins          #+#    #+#             */
-/*   Updated: 2024/10/02 17:52:28 by btomlins         ###   ########.fr       */
+/*   Updated: 2024/10/02 17:54:12 by btomlins         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,6 +19,10 @@ int main(int argc, char **argv)
     
     j = -1;
     if (argc < 2)
-            std:cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *";
+            std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *";
+    while (argc > 1 && argv && argv[++j])
+    {
+        i = -1;
+    }
     return (0);
 }
