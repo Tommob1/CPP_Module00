7f6cd07d36679fe1eb15f32a9e9619224f8c033e
Brayden Tomlinson <humortronic@gmail.com>
2024-10-23 13:08:36 +1030

    Added changes to code

---
+++ ex01/Contact.hpp
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Contact.hpp                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: btomlins <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/23 13:00:00 by btomlins          #+#    #+#             */
+/*   Updated: 2024/10/23 13:08:17 by btomlins         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef CONTACT_HPP
+#define CONTACT_HPP
+
+#include <string>
+
+class Contact {
+    private:
+        std::string first_name;
+        std::string last_name;
+        std::string nickname;
+        std::string phone_number;
+        std::string darkest_secret;
+
+    public:
+        Contact();
+}
