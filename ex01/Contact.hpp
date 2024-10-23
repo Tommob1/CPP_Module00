9c0200d9ed8f78d6ce1bf105982330b9fd461d1f
Brayden Tomlinson <humortronic@gmail.com>
2024-10-23 13:59:52 +1030

    Added changes to code

--- ex01/Contact.hpp
+++ ex01/Contact.hpp
@@ -6,14 +6,15 @@
 /*   By: btomlins <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/23 13:00:00 by btomlins          #+#    #+#             */
-/*   Updated: 2024/10/23 13:53:28 by btomlins         ###   ########.fr       */
+/*   Updated: 2024/10/23 13:56:57 by btomlins         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef CONTACT_H
-# define CONTACT_H
+#define CONTACT_H
 #include <string>
 #include <iostream>
+#include <iomanip>
 #include <ctime>
 
 class Contact
