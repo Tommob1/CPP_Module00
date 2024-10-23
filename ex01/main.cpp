d512b394e6f7db2eba0c11688f1b23950c784e4a
Brayden Tomlinson <humortronic@gmail.com>
2024-10-23 14:04:38 +1030

    Added changes to code

--- ex01/main.cpp
+++ ex01/main.cpp
@@ -6,7 +6,17 @@
 /*   By: btomlins <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/23 13:59:59 by btomlins          #+#    #+#             */
-/*   Updated: 2024/10/23 13:59:59 by btomlins         ###   ########.fr       */
+/*   Updated: 2024/10/23 14:04:24 by btomlins         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <iostream>
+#include "PhoneBook.hpp"
+
+int main()
+{
+    PhoneBook phonebook;
+    std::string command;
+
+    return (0);
+}
