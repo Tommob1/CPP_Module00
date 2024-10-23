c1d3d61c43f7e3ee263f5392faff2a9c47ea9dfa
Brayden Tomlinson <humortronic@gmail.com>
2024-10-23 13:50:55 +1030

    Added changes to code

--- ex01/PhoneBook.hpp
+++ ex01/PhoneBook.hpp
@@ -6,8 +6,18 @@
 /*   By: btomlins <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/22 13:22:50 by btomlins          #+#    #+#             */
-/*   Updated: 2024/10/22 13:23:02 by btomlins         ###   ########.fr       */
+/*   Updated: 2024/10/23 13:50:38 by btomlins         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <iostream>
+#ifndef PHONEBOOK_H
+#define PHONEBOOK_H
+
+#include "Contact.hpp"
+
+class PhoneBook
+{
+    private:
+        
+    public:
+}
