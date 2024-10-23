2e32c632a924252496e72ce2389ff06180cb50f2
Brayden Tomlinson <humortronic@gmail.com>
2024-10-23 14:25:51 +1030

    Added changes to code

--- ex01/PhoneBook.cpp
+++ ex01/PhoneBook.cpp
@@ -6,7 +6,7 @@
 /*   By: btomlins <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/09 16:36:35 by btomlins          #+#    #+#             */
-/*   Updated: 2024/10/23 14:10:04 by btomlins         ###   ########.fr       */
+/*   Updated: 2024/10/23 14:25:36 by btomlins         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,6 +23,34 @@ void PhoneBook::addContact(const Contact& newContact)
     }
     else
     {
-        
+        contacts[oldestIndex] = newContact;
+        oldestIndex = (oldestIndex + 1) % 8;
+    }
+}
+
+void PhoneBook::searchContacts() const
+{
+    int i;
+    i = 0;
+    
+    if (contactCount == 0)
+    {
+        std::cout << "No contacts saved." << std::endl;
+        return;
+    }
+
+    std::cout << std::setw(10) << "Index" << "|"
+              << std::setw(10) << "First Name" << "|"
+              << std::setw(10) << "Last Name" << "|"
+              << std::setw(10) << "Nickname" << std::endl;
+    std::cout << "---------------------------------------------"
+
+    while (i < contactCount)
+    {
+        std::cout << std::setw(10) << i + 1 << "|"
+                << std::setw(10) << (contacts[i].getFirst_name().length() > 10 ? contacts[i].getFirst_name().substr(0, 9) + "." : contacts[i].getFirst_name()) << "|"
+                << std::setw(10) << (contacts[i].getLastName().length() > 10 ? contacts[i].getLastName().substr(0, 9) + "." : contacts[i].getLastName()) << "|"
+                << std::setw(10) << (contacts[i].getNick_name().length() > 10 ? contacts[i].getNick_name().substr(0, 9) + "." : contacts[i].getNick_name())
+                << std::endl;
     }
 }
