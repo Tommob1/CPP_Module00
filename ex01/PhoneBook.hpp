b3e23cb26cbce426db34d4f277f7b3805aabc472
Brayden Tomlinson <humortronic@gmail.com>
2024-10-23 15:02:32 +1030

    Added changes to code

--- ex01/PhoneBook.hpp
+++ ex01/PhoneBook.hpp
@@ -6,7 +6,7 @@
 /*   By: btomlins <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/22 13:22:50 by btomlins          #+#    #+#             */
-/*   Updated: 2024/10/23 14:58:14 by btomlins         ###   ########.fr       */
+/*   Updated: 2024/10/23 15:02:14 by btomlins         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,8 +26,7 @@ class PhoneBook
 
     public:
         PhoneBook();
-        void addContact(std::string first_name, std::string last_name, std::string nick_name,
-                    std::string phone_number, std::string darkest_secret);
+        void addContact(const Contact &newContact); 
         void searchContacts() const;
         int searchOldestContact() const;
 };
