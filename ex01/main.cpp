d2a9525510c1ebe9933e6fe98098ccbdf92017b8
Brayden Tomlinson <humortronic@gmail.com>
2024-10-23 15:10:45 +1030

    Added changes

--- ex01/main.cpp
+++ ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: btomlins <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/23 13:59:59 by btomlins          #+#    #+#             */
-/*   Updated: 2024/10/23 14:06:39 by btomlins         ###   ########.fr       */
+/*   Updated: 2024/10/23 15:10:30 by btomlins         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,7 +20,27 @@ int main()
 
     while (true)
     {
-        
+        std::cout << "Enter a command (ADD, SEARCH, EXIT): ";
+        std::cin >> command;
+
+        if (command == "ADD")
+        {
+            std::string first_name, last_name, nickname, phone_number, darkest_secret;
+
+            std::cout << "First name: ";
+            std::cin >> first_name;
+            std::cout << "Last name: ";
+            std::cin >>last_name;
+            std::cout << "Nickname: ";
+            std::cin >> nickname;
+            std::cout << "Phone number: ";
+            std::cin >> phone_number;
+            std::cout << "Darkest secret: ";
+            std::cin >> darkest_secret;
+
+            Contact newContact(first_name, last_name, nickname, phone_number, darkest_secret);
+            phonebook.addContact(newContact);
+        }
     }
     return (0);
 }
