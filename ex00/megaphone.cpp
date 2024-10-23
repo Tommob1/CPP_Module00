83ab1ca6568a27ebc04264ce3efd699f000b223a
Brayden Tomlinson <btomlins@F2K016.42adel.org.au>
2024-10-23 12:18:56 +1030

    Added changes to code:

--- ex00/megaphone.cpp
+++ ex00/megaphone.cpp
@@ -6,11 +6,7 @@
 /*   By: btomlins <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/01 16:38:37 by btomlins          #+#    #+#             */
-<<<<<<< HEAD
-/*   Updated: 2024/10/02 17:47:53 by btomlins         ###   ########.fr       */
-=======
-/*   Updated: 2024/10/02 17:58:19 by btomlins         ###   ########.fr       */
->>>>>>> 96bd954eda31c1a97e175bd0c01f690ce2399c67
+/*   Updated: 2024/10/23 12:18:28 by btomlins         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,14 +19,6 @@ int main(int argc, char **argv)
     
     j = -1;
     if (argc < 2)
-<<<<<<< HEAD
-            std::cout << "* LOUD AND UNBEARBALE FEEDBACK NOISE *";
-    while (argc > 1 && argv && argv[++j])
-    {
-        
-    }
-
-=======
             std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *";
     while (argc > 1 && argv && argv[++j])
     {
@@ -39,6 +27,5 @@ int main(int argc, char **argv)
                 std::cout << (char)std::toupper(argv[j][i]);
     }
     std::cout << std::endl;
->>>>>>> 96bd954eda31c1a97e175bd0c01f690ce2399c67
     return (0);
 }
