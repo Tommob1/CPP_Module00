f6ed53e99954fce0d33fec3fb95e88f5a7f4223c
Brayden Tomlinson <humortronic@gmail.com>
2024-10-24 12:14:30 +1030

    Added changes to ocde

--- ex02/Account.cpp
+++ ex02/Account.cpp
@@ -6,11 +6,16 @@
 /*   By: btomlins <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/23 15:51:08 by btomlins          #+#    #+#             */
-/*   Updated: 2024/10/24 12:10:26 by btomlins         ###   ########.fr       */
+/*   Updated: 2024/10/24 12:13:06 by btomlins         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Account.hpp"
 #include <ctime>
 #include <iostream>
+
+int Account::_nbAccounts = 0;
+int Account::_totalAmount = 0;
+int Account::_totalNbDeposits = 0;
+int Account::_totalNbWithdrawals = 0;
 
