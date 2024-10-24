f3e30d59e5d9031f45bf28ea7c920b7bda8cb03e
Brayden Tomlinson <humortronic@gmail.com>
2024-10-24 12:20:16 +1030

    Added changes to code'

--- ex02/Account.cpp
+++ ex02/Account.cpp
@@ -6,7 +6,7 @@
 /*   By: btomlins <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/23 15:51:08 by btomlins          #+#    #+#             */
-/*   Updated: 2024/10/24 12:13:06 by btomlins         ###   ########.fr       */
+/*   Updated: 2024/10/24 12:19:42 by btomlins         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,3 +19,9 @@ int Account::_totalAmount = 0;
 int Account::_totalNbDeposits = 0;
 int Account::_totalNbWithdrawals = 0;
 
+Account::Account(int initial_deposit) :_accountIndex(_nbAccounts), _amount(initial_deposit), _nbDeposits(0), _nbWithdrawals(0)
+{
+    _nbAccounts++;
+    _totalAmount += initial_deposit;
+    std::cout << "Account created. Index: " << _accountIndex << ", Initial Deposit: " << _amount << std::endl;
+}
